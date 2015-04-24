d7bf57c321031ad4db1dbee6fa1e2cc72ce16ca0
CS_Toku <cit-h8-toku@mail.h8-ttt.com>
2015-04-24 17:58:41 +0900

    入力機能追加

--- calculatorwindow.cpp
+++ calculatorwindow.cpp
@@ -6,9 +6,30 @@ CalculatorWindow::CalculatorWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::CalculatorWindow)
 {
-    ui->setupUi(this);
-    QObject::connect(ui->pushButton_20, SIGNAL(clicked()), this, SLOT(a()));
+    this->isInitialized = false;
+    this->fontAspectRaito = 0;
 
+    ui->setupUi(this);
+
+    QObject::connect(ui->EqualButton, SIGNAL(clicked()), this, SLOT(a()));
+    QObject::connect(ui->NumButton_0, SIGNAL(clicked()), this, SLOT(addNumberZero()));
+    QObject::connect(ui->NumButton_1, SIGNAL(clicked()), this, SLOT(addNumberOne()));
+    QObject::connect(ui->NumButton_2, SIGNAL(clicked()), this, SLOT(addNumberTwo()));
+    QObject::connect(ui->NumButton_3, SIGNAL(clicked()), this, SLOT(addNumberThree()));
+    QObject::connect(ui->NumButton_4, SIGNAL(clicked()), this, SLOT(addNumberFour()));
+    QObject::connect(ui->NumButton_5, SIGNAL(clicked()), this, SLOT(addNumberFive()));
+    QObject::connect(ui->NumButton_6, SIGNAL(clicked()), this, SLOT(addNumberSix()));
+    QObject::connect(ui->NumButton_7, SIGNAL(clicked()), this, SLOT(addNumberSeven()));
+    QObject::connect(ui->NumButton_8, SIGNAL(clicked()), this, SLOT(addNumberEight()));
+    QObject::connect(ui->NumButton_9, SIGNAL(clicked()), this, SLOT(addNumberNine()));
+    QObject::connect(ui->PointButton, SIGNAL(clicked()), this, SLOT(addDecimalPoint()));
+    QObject::connect(ui->RBracketButton, SIGNAL(clicked()), this, SLOT(addRightBracket()));
+    QObject::connect(ui->LBracketButton, SIGNAL(clicked()), this, SLOT(addLeftBracket()));
+    QObject::connect(ui->ClearButton, SIGNAL(clicked()), this, SLOT(ClearData()));
+    QObject::connect(ui->AddButton, SIGNAL(clicked()), this, SLOT(addAddSymbol()));
+    QObject::connect(ui->SubtractButton, SIGNAL(clicked()), this, SLOT(addSubtractSymbol()));
+    QObject::connect(ui->MultiplyButton, SIGNAL(clicked()), this, SLOT(addMultiplySymbol()));
+    QObject::connect(ui->DivideButton, SIGNAL(clicked()), this, SLOT(addDivideSymbol()));
 
 }
 
@@ -17,6 +38,59 @@ CalculatorWindow::~CalculatorWindow()
 
     delete ui;
 }
+
+void CalculatorWindow::addNumber(QString num){
+    QString value = ui->valueDisplay->text();
+    if (value.remove(".").length() >= 20) return;
+    value = ui->valueDisplay->text();
+    if (value == "0")
+        ui->valueDisplay->setText(num);
+    else
+        ui->valueDisplay->setText(value+num);
+    this->resizeFont();
+}
+
+void CalculatorWindow::addDecimalPoint(void){
+    QString value = ui->valueDisplay->text();
+    if (value.indexOf(".") < 0)
+        ui->valueDisplay->setText(value+".");
+}
+
+void CalculatorWindow::addLeftBracket(void){
+    ui->valueDisplay->setText(ui->valueDisplay->text()+"(");
+}
+void CalculatorWindow::addRightBracket(void){
+    ui->valueDisplay->setText(ui->valueDisplay->text()+")");
+}
+
+void CalculatorWindow::ClearData(void){
+    ui->valueDisplay->setText("0");
+}
+
+void CalculatorWindow::addAddSymbol(void){ this->addSymbol("＋"); }
+void CalculatorWindow::addSubtractSymbol(void){ this->addSymbol("−"); }
+void CalculatorWindow::addMultiplySymbol(void){ this->addSymbol("×"); }
+void CalculatorWindow::addDivideSymbol(void){ this->addSymbol("÷"); }
+
+void CalculatorWindow::addSymbol(QString symbol){
+    QString value = ui->valueDisplay->text();
+    ui->formulaDisplay->setText(ui->formulaDisplay->text() + value + symbol);
+    ui->valueDisplay->setText("0");
+}
+
+void CalculatorWindow::addNumberZero(void){ this->addNumber("0"); }
+void CalculatorWindow::addNumberOne(void){ this->addNumber("1"); }
+void CalculatorWindow::addNumberTwo(void){ this->addNumber("2"); }
+void CalculatorWindow::addNumberThree(void){ this->addNumber("3"); }
+void CalculatorWindow::addNumberFour(void){ this->addNumber("4"); }
+void CalculatorWindow::addNumberFive(void){ this->addNumber("5"); }
+void CalculatorWindow::addNumberSix(void){ this->addNumber("6"); }
+void CalculatorWindow::addNumberSeven(void){ this->addNumber("7"); }
+void CalculatorWindow::addNumberEight(void){ this->addNumber("8"); }
+void CalculatorWindow::addNumberNine(void){ this->addNumber("9"); }
+
+
+
 
 bool CalculatorWindow::event(QEvent *event){
 
@@ -32,34 +106,46 @@ bool CalculatorWindow::event(QEvent *event){
 
 
 void CalculatorWindow::windowStateChangeEvent(QWindowStateChangeEvent* event){
-    std::cout<<"Hey!!"<<std::endl;
+    std::cout << "window" << std::endl;
     this->resizeFont();
 }
 
 void CalculatorWindow::resizeEvent(QResizeEvent* event){
+    std::cout << "resize" << std::endl;
     this->resizeFont();
 }
 
 
 void CalculatorWindow::showEvent(QShowEvent *event){
+    std::cout << "show" << std::endl;
+    this->resizeFont();
+
     if(this->isInitialized)
         return;
-    this->resizeFont();
+    QLabel *vd = ui->valueDisplay;
+    this->fontAspectRaito = vd->font().pixelSize()/(vd->width()/(double)vd->text().length());
     this->isInitialized = true;
 }
 
 void CalculatorWindow::resizeFont(void){
-    double allstretch=0;
+    double allstretch=0, raito=0;
+    int px=0, real_width=0;
     QBoxLayout* layout = (QBoxLayout*)ui->centralWidget->layout();
     for(int i=0; i < layout->count(); i++){
         allstretch += layout->stretch(i);
     }
 
-    double raito = layout->stretch(layout->indexOf(ui->valueWidget))/allstretch;
-    int px = ui->centralWidget->height()*raito*3/4.0;
+    //ValueDisplay
+    raito = layout->stretch(layout->indexOf(ui->valueWidget))/allstretch;
+    px = ui->centralWidget->height()*raito*3/4.0;
+    real_width = px/this->fontAspectRaito*ui->valueDisplay->text().length();
+    if(real_width > ui->centralWidget->width())
+        px = ui->centralWidget->width()/(double)ui->valueDisplay->text().length()*this->fontAspectRaito;
     QFont font = ui->valueDisplay->font();
     font.setPixelSize(px);
     ui->valueDisplay->setFont(font);
+
+    //FormulaDisplay
     raito = layout->stretch(layout->indexOf(ui->formulaWidget))/allstretch;
     px = ui->centralWidget->height()*raito*3/4.0;
     font = ui->formulaDisplay->font();
@@ -69,6 +155,4 @@ void CalculatorWindow::resizeFont(void){
 
 void CalculatorWindow::a(){
 
-    std::cout <<  ui->valueDisplay->width() << std::endl;
-    std::cout <<  ui->centralWidget->width() << std::endl;
 }
--- calculatorwindow.h
+++ calculatorwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QResizeEvent>
 #include <QSize>
+#include <QGraphicsTextItem>
 
 namespace Ui {
 class CalculatorWindow;
@@ -19,17 +20,42 @@ public:
 
     void resizeFont(void);
 
+
     bool event(QEvent *event);
     void windowStateChangeEvent(QWindowStateChangeEvent* event);
     void resizeEvent(QResizeEvent* event);
     void showEvent(QShowEvent *event);
 
 public slots:
-    void a();
+    void a(void);
+    void addNumberZero(void);
+    void addNumberOne(void);
+    void addNumberTwo(void);
+    void addNumberThree(void);
+    void addNumberFour(void);
+    void addNumberFive(void);
+    void addNumberSix(void);
+    void addNumberSeven(void);
+    void addNumberEight(void);
+    void addNumberNine(void);
+    void addDecimalPoint(void);
+    void addRightBracket(void);
+    void addLeftBracket(void);
+    void ClearData(void);
+    void addAddSymbol(void);
+    void addSubtractSymbol(void);
+    void addMultiplySymbol(void);
+    void addDivideSymbol(void);
 
 private:
     Ui::CalculatorWindow *ui;
     bool isInitialized;
+    double fontAspectRaito;
+    QString *formulaStr;
+    QString *valueStr;
+
+    void addNumber(QString num);
+    void addSymbol(QString symbol);
 };
 
 #endif // CALCULATORWINDOW_H
